d3e830bfa2127bfd46438db4c2377959dda9b33f
santosh0208 <santoshalamuru0@msitprogram.net>
2025-06-19 17:03:50 +0530

    all

---
+++ App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { View, StyleSheet, Text } from 'react-native';
+
+function App() {
+  
+
+  return (
+    <View style={styles.container}>
+      <Text>
+        Hello Vydhyo Doctor! hello
+        </Text> 
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
